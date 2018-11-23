74cd2ee9d3ec60fc3d076f23e2a0c51b0568f37b
piresp <paulo.albert.pires@gmail.com>
2018-11-23 15:52:35 -0200

    Commit Aula02

--- src/br/com/casadocodigo/loja/config/ServletSpringMvc.java
+++ src/br/com/casadocodigo/loja/config/ServletSpringMvc.java
@@ -2,22 +2,21 @@ package br.com.casadocodigo.loja.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-//configuracao do Servlet do Spring MVC para que ela atenda as requisições recebidas pelo servidor. 
 public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer{
 
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return null;
-    }
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
 
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[] { AppWebConfiguration.class};
-    }
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {AppWebConfiguration.class, JPAConfiguration.class};
+	}
 
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] {"/"};
-    }
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
 
 }
