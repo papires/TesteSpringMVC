4b4903ddd17dbb1710b923b2cbe444d8c98a83c8
piresp <paulo.albert.pires@gmail.com>
2018-11-24 10:21:22 -0200

    commit Aula 4

--- WebContent/WEB-INF/views/produtos/form.jsp
+++ WebContent/WEB-INF/views/produtos/form.jsp
@@ -1,12 +1,10 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=UTF-8"	pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Livros de Java, Android, iPhone, PHP, Ruby e muito mais -
-	Casa do Código</title>
+	<meta charset="UTF-8">
+	<title>Livros de Java, Android, iPhone, PHP, Ruby e muito mais - Casa do Código</title>
 </head>
 <body>
 	<form action="/CasaTeste/produtos" method="POST">
@@ -24,7 +22,8 @@
 			<div>
 				<label>${tipoPreco}</label> 
 				<input type="text"   name="precos[${status.index}].valor"> 
-				<input type="hidden" name="precos[${status.index}].tipo" value="${tipoPreco}">
+				<input type="hidden" name="precos[${status.index}].tipo"
+					 value="${tipoPreco}">
 			</div>
 		</c:forEach>
 		<button type="submit">Cadastrar</button>		
--- src/br/com/casadocodigo/loja/DAO/ProdutoDAO.java
+++ src/br/com/casadocodigo/loja/DAO/ProdutoDAO.java
@@ -1,22 +1,48 @@
 package br.com.casadocodigo.loja.DAO;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Repository
 @Transactional
 public class ProdutoDAO {
 
-	@PersistenceContext
-	private EntityManager manager;
-	
-	public void gravar(Produto produto) {
-		manager.persist(produto);
-	}
-	
+    @Autowired
+    private ProdutoDAO produtoDao;
+
+    @PersistenceContext
+    private EntityManager manager;
+
+
+    
+    @RequestMapping("form")
+    public ModelAndView form() {
+        ModelAndView modelAndView = new ModelAndView("produtos/form");
+        modelAndView.addObject("tipos", TipoPreco.values());
+        return modelAndView;
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public void gravar(Produto produto) {
+        manager.persist(produto);
+    }
+
+    public List<Produto> listar() {
+        return manager.createQuery("select p from Produto p", Produto.class)
+            .getResultList();
+    }
+
+    
 }
